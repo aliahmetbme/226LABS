77f3046b6f5cb2031dd08e312cd4f9b08c7a0b09
ALİ AHMET ERDOĞDU <082aliahmet4@gmail.com>
2026-02-26 12:12:06 +0300

    first commit

---
+++ lab1/main.py
@@ -0,0 +1,38 @@
+
+name = input("What is your name?")
+print("Hello "+name +".")
+id = input("What is your Student ID?")
+print("Your ID is "+id+".")
+
+#**************************************************
+
+var1 = int(input("Enter a value: "))
+var2 = int(input("Enter a second value: "))
+
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+
+print("First Value: "+ str(var1))
+print("Second Value: "+ str(var2))
+print("Summation: "+ str(sum))
+print("Difference: "+ str(diff))
+print("Product: "+ str(prod))
+
+#*************************************************
+name = input("What is your name?")
+lab = int(input("Lab grade?"))
+midterm = int(input("Midterm grade? "))
+final = int(input("Final grade? "))
+
+score = lab*0.25 + midterm*0.35 + final*0.40
+
+print("Name      : "+str(name))
+print("Lab       : "+str(lab))
+print("Midterm   : "+str(midterm))
+print("Final     : "+str(final))
+print("Last Score: "+str(score))
+
+#***********************************************
+
+print("*\n"+"**\n"+"***\n"+"**\n"+"*\n")
